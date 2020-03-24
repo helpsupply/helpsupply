d03b5b401262373c74380dd070ba8f50ce90a952
Izzy George <george@work.co>
2020-03-24 11:07:10 -0700

    update button styles and add to styleguide

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,9 +3,9 @@ import { jsx, css } from '@emotion/core'
 import { TEXT_TYPE, textStyles } from "./Text.styles"
 
 function Text({ children, type = TEXT_TYPE.BODY_1, as: Tag = 'span' }) {
-    return (
-        <Tag css={css(textStyles[type])}>{children}</Tag>
-    );
-  }
+  return (
+    <Tag css={css(textStyles[type])}>{children}</Tag>
+  );
+}
 
 export default Text;
