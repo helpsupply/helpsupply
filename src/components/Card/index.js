da25c2c68719ee4c6f9304546480685a28da03db
Liz Banach <erbanach@gmail.com>
2020-03-24 16:47:54 -0700

    card component + refactor text component

---
+++ src/components/Card/index.js
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import PropTypes from 'prop-types'
+import { SecondaryButton } from 'components/Button'
+import Text from 'components/Text'
+import { TEXT_TYPE } from 'components/Text/constants'
+import { styles } from './Card.styles'
+
+export const Card = ({ details, onClick, label }) => (
+  <div css={styles.root}>
+      <div>
+        <Text as='p' type={TEXT_TYPE.NOTE} styles={styles.label}>{label}</Text>
+        <Text as='p'>{details}</Text>
+      </div>
+      <SecondaryButton onClick={onClick}>
+        <Text type={TEXT_TYPE.NOTE}>Edit</Text>
+      </SecondaryButton>
+  </div>
+)
+
+Card.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+}
+
+export default Card
