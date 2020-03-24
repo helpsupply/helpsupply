3ffa331bbb3ae2dd6d437dd5b24d4fc017a5765c
Izzy George <george@work.co>
2020-03-24 11:07:10 -0700

    update button styles and add to styleguide

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,47 +1,27 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import PropTypes from 'prop-types'
-import { radius, type } from "./constants"
+import { type } from "./constants"
 
 import styles from './Button.styles'
 
-export const PrimaryRoundedButton = ({ children, ...rest }) => (
-  <button css={[styles.root, styles[type.FILL], styles[radius.ROUNDED]]} {...rest}>
+export const PrimaryButton = ({ children, ...rest }) => (
+  <button css={[styles.root, styles[type.FILL]]} {...rest}>
     {children}
   </button>
 )
 
-PrimaryRoundedButton.propTypes = {
+PrimaryButton.propTypes = {
   onClick: PropTypes.func.isRequired,
 }
 
-export const PrimaryPillButton = ({ children, ...rest }) => (
-  <button css={[styles.root, styles[type.FILL], styles[radius.PILL]]} {...rest}>
+export const SecondaryButton = ({ children, ...rest }) => (
+  <button css={[styles.root, styles[type.OUTLINE]]} {...rest}>
     {children}
   </button>
 )
 
-PrimaryPillButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-export const SecondaryRoundedButton = ({ children, ...rest }) => (
-  <button css={[styles.root, styles[type.OUTLINE], styles[radius.ROUNDED]]} {...rest}>
-    {children}
-  </button>
-)
-
-SecondaryRoundedButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-export const SecondaryPillButton = ({ children, ...rest }) => (
-  <button css={[styles.root, styles[type.OUTLINE], styles[radius.PILL]]} {...rest}>
-    {children}
-  </button>
-)
-
-SecondaryPillButton.propTypes = {
+SecondaryButton.propTypes = {
   onClick: PropTypes.func.isRequired,
 }
 
--- src/components/StyleGuide.js
+++ src/components/StyleGuide.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { jsx } from '@emotion/core'
 import Text from './Text/Text'
 import { TEXT_TYPE } from './Text/Text.styles'
+import { PrimaryButton, SecondaryButton } from './Button'
+import InputText from './InputText'
 
 class StyleGuide extends React.Component {
   render() {
     return (
-      <div css={{ maxWidth: '1200px', width: '100%', margin: '0 auto', padding: '1em'}}>
+      <div css={{ maxWidth: '1200px', width: '100%', margin: '0 auto', padding: '1em' }}>
         <h1>Style Guide</h1>
         <hr />
         <Text as='h1' type={TEXT_TYPE.HEADER_1}>Header 1</Text>
@@ -16,8 +18,28 @@ class StyleGuide extends React.Component {
         <Text as='h4' type={TEXT_TYPE.HEADER_4}>Header 4</Text>
         <Text as='p' type={TEXT_TYPE.BODY_1}>Body 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
         <Text as='p' type={TEXT_TYPE.BODY_2}>Body 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
-        <Text as='p' type={TEXT_TYPE.NOTE}>Body 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
+        <Text as='p' >Body 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
         <hr />
+        <div css={{ width: 500, '> div': { marginBottom: 10 } }}>
+          <div>
+            <PrimaryButton><Text>Primary Button</Text></PrimaryButton>
+          </div>
+          <div>
+            <PrimaryButton disabled><Text>Primary Button Disabled</Text></PrimaryButton>
+          </div>
+          <div>
+            <SecondaryButton><Text type={TEXT_TYPE.NOTE}>Secondary Button</Text></SecondaryButton>
+          </div>
+          <div>
+            <SecondaryButton disabled><Text type={TEXT_TYPE.NOTE}>Secondary Button Disabled</Text></SecondaryButton>
+          </div>
+        </div>
+        <hr />
+        <div css={{ width: 500, '> div': { marginBottom: 10 } }}>
+          <div>
+            <InputText label="Label" />
+          </div>
+        </div>
       </div>
     );
   }
