84706053f5274f2a2edd8d89b878e7690b834dc0
Izzy George <george@work.co>
2020-03-23 21:11:06 -0700

    create basic input text

---
+++ src/components/InputText/index.js
@@ -0,0 +1,40 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react'
+
+import styles from './InputText.styles'
+
+class InputText extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isFocused: false,
+      value: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ value: e.target.value })
+  }
+
+  toggleFocus = () => {
+    this.setState({ isFocused: !this.state.isFocused })
+  }
+
+  render() {
+    const { isFocused, value } = this.state
+    const { label, ...rest } = this.props
+
+    return (
+      <label css={[styles.root, value && styles.active]}>
+        {label && (
+          <div css={[styles.label, (isFocused || value) && styles.activeLabel]}>{label}</div>
+        )}
+        <input css={styles.input} onBlur={this.toggleFocus} onFocus={this.toggleFocus} onChange={this.onChange} value={value} {...rest} />
+      </label>
+    );
+  }
+}
+
+export default InputText
