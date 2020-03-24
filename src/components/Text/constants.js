bd228f3470257fb52c982ae70193dca41b994eee
Liz Banach <erbanach@gmail.com>
2020-03-24 16:47:54 -0700

    card component + refactor text component

--- src/components/Text/constants.js
+++ src/components/Text/constants.js
@@ -1,9 +1,9 @@
 export const TEXT_TYPE = {
-    'HEADER_1': 'HEADER_1',
-    'HEADER_2': 'HEADER_2',
-    'HEADER_3': 'HEADER_3',
-    'HEADER_4': 'HEADER_4',
-    'BODY_1': 'BODY_1',
-    'BODY_2': 'BODY_2',
-    'NOTE': 'NOTE',
+    HEADER_1: 'HEADER_1',
+    HEADER_2: 'HEADER_2',
+    HEADER_3: 'HEADER_3',
+    HEADER_4: 'HEADER_4',
+    BODY_1: 'BODY_1',
+    BODY_2: 'BODY_2',
+    NOTE: 'NOTE',
   }
