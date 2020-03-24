2a994670713c6e40d31079fea34eb1a3340c0c0a
Liz Banach <erbanach@gmail.com>
2020-03-24 15:06:47 -0700

    fix text component + anchor link

---
+++ src/components/Anchor/index.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import PropTypes from 'prop-types'
+import { styles } from './Anchor.styles'
+
+export const Anchor = ({ children, ...rest }) => (
+  <a css={styles.root} {...rest}>
+    {children}
+  </a>
+)
+
+Anchor.propTypes = {
+  href: PropTypes.string.isRequired,
+}
+
+export default Anchor
