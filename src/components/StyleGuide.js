ca657130e1a88c8d07758f31bd187858cde60faa
Liz Banach <erbanach@gmail.com>
2020-03-23 20:47:00 -0700

    feat: text component and styles

--- src/components/StyleGuide.js
+++ src/components/StyleGuide.js
@@ -1,38 +1,13 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from '@emotion/core'
-import { TEXT_TYPE } from './Text/constants'
-import Text from './Text'
-import { PrimaryButton, SecondaryButton } from './Button'
-import InputText from './InputText'
-import Autosuggest from './Autosuggest'
-import TextArea from './TextArea'
-import Anchor from './Anchor'
-import InputDropdown from './InputDropdown'
-import Card from './Card'
+import Text from './Text/Text'
+import { TEXT_TYPE } from './Text/Text.styles'
 
 class StyleGuide extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      facilities: []
-    }
-  }
-
-  componentDidMount() {
-    this.props.backend.listDropSites().then(data => {
-      const facilities = data.map((facility) => ({
-        name: facility.dropSiteName,
-        address: facility.dropSiteAddress,
-        id: facility.id
-      }))
-      this.setState({ facilities })
-    })
-  }
-
   render() {
     return (
-      <div css={{ maxWidth: '1200px', width: '100%', margin: '0 auto', padding: '1em' }}>
+      <div css={{ maxWidth: '1200px', width: '100%', margin: '0 auto', padding: '1em'}}>
         <h1>Style Guide</h1>
         <hr />
         <Text as='h1' type={TEXT_TYPE.HEADER_1}>Header 1</Text>
@@ -41,43 +16,11 @@ class StyleGuide extends React.Component {
         <Text as='h4' type={TEXT_TYPE.HEADER_4}>Header 4</Text>
         <Text as='p' type={TEXT_TYPE.BODY_1}>Body 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
         <Text as='p' type={TEXT_TYPE.BODY_2}>Body 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
-        <Text as='p' type={TEXT_TYPE.NOTE}>Note. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
-        <Anchor href="/">Anchor Link</Anchor>
+        <Text as='p' type={TEXT_TYPE.NOTE}>Body 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nunc faucibus neque finibus ultrices a non risus.</Text>
         <hr />
-        <div css={{ width: 500, '> div': { marginBottom: 10 } }}>
-          <div>
-            <PrimaryButton onClick={() => false}><Text>Primary Button</Text></PrimaryButton>
-          </div>
-          <div>
-            <PrimaryButton disabled onClick={() => false}><Text>Primary Button Disabled</Text></PrimaryButton>
-          </div>
-          <div>
-            <SecondaryButton onClick={() => false}><Text type={TEXT_TYPE.NOTE}>Secondary Button</Text></SecondaryButton>
-          </div>
-          <div>
-            <SecondaryButton disabled onClick={() => false}><Text type={TEXT_TYPE.NOTE}>Secondary Button Disabled</Text></SecondaryButton>
-          </div>
-        </div>
-        <hr />
-        <div css={{ width: 500, '> div': { marginBottom: 15 } }}>
-          <div>
-            <InputText label="Label" />
-          </div>
-          <div>
-            <Autosuggest label="City or medical facility" suggestions={this.state.facilities} />
-          </div>
-          <div>
-            <InputDropdown placeholder="Select supply type" options={[{ label: 'Foo', value: 'foo' }, { label: 'Bar', value: 'bar' }]} />
-          </div>
-          <div>
-            <TextArea label="i.e.: All donated items must be unused and sealed in original packaging." />
-          </div>
-        </div>
-        <hr />
-        <Card onClick={() => false} label="Donation requirements" details="Unused, in original packaging" />
       </div>
     );
   }
 }
 
-export default StyleGuide
+export default StyleGuide;
--- src/components/Text/Text.styles.js
+++ src/components/Text/Text.styles.js
@@ -1,53 +1,58 @@
-import { css } from '@emotion/core'
-import { TEXT_TYPE } from './constants'
-
 export const Font = `"Inter", Helvetica, Arial, sans-serif`
 
-export const textStyles = {
-  [TEXT_TYPE.HEADER_1]: css({
-    fontFamily: Font,
-    fontSize: 40,
-    lineHeight: '34px',
-    letterSpacing: '-0.04em',
-  }),
-  [TEXT_TYPE.HEADER_2]: css({
-    fontFamily: Font,
-    fontSize: 30,
-    fontWeight: 600,
-    lineHeight: '34px',
-  }),
-  [TEXT_TYPE.HEADER_3]: css({
-    fontFamily: Font,
-    fontSize: 26,
-    fontWeight: 600,
-    lineHeight: '32px',
-    letterSpacing: '-0.04em',
-  }),
-  [TEXT_TYPE.HEADER_4]: css({
-    fontFamily: Font,
-    fontSize: 24,
-    fontWeight: 500,
-    lineHeight: '26px',
-    letterSpacing: '-0.03em',
-  }),
-  [TEXT_TYPE.BODY_1]: css({
-    fontFamily: Font,
-    fontSize: 16,
-    lineHeight: '24px',
-    letterSpacing: '-0.03em',
-  }),
-  [TEXT_TYPE.BODY_2]: css({
-    fontFamily: Font,
-    fontSize: 14,
-    lineHeight: '22px',
-    letterSpacing: '-0.01em',
-  }),
-  [TEXT_TYPE.NOTE]: css({
-    fontFamily: Font,
-    fontSize: 12,
-    lineHeight: '18px',
-    letterSpacing: '-0.01em',
-  }),
+export const TEXT_TYPE = {
+  'HEADER_1': 'HEADER_1',
+  'HEADER_2': 'HEADER_2',
+  'HEADER_3': 'HEADER_3',
+  'HEADER_4': 'HEADER_4',
+  'BODY_1': 'BODY_1',
+  'BODY_2': 'BODY_2',
+  'NOTE': 'NOTE',
 }
 
-export default textStyles
+export const textStyles = {
+  [TEXT_TYPE.HEADER_1]: `
+    font-family: ${Font};
+    font-size: 40px;
+    line-height: 38px;
+    letter-spacing: -0.04em;
+  `,
+  [TEXT_TYPE.HEADER_2]: `
+    font-family: ${Font};
+    font-size: 30px;
+    font-weight: 600;
+    line-height: 34px;
+  `,
+  [TEXT_TYPE.HEADER_3]: `
+    font-family: ${Font};
+    font-size: 26px;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: -0.04em;
+  `,
+  [TEXT_TYPE.HEADER_4]: `
+    font-family: ${Font};
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 26px;
+    letter-spacing: -0.03em;
+  `,
+  [TEXT_TYPE.BODY_1]: `
+    font-family: ${Font};
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.03em;
+  `,
+  [TEXT_TYPE.BODY_2]: `
+    font-family: ${Font};
+    font-size: 14px;
+    line-height: 22px;
+    letter-spacing: -0.01em;
+  `,
+  [TEXT_TYPE.NOTE]: `
+  font-family: ${Font};
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: -0.01em;
+`,
+}
