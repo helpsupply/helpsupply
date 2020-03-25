6c5dcf0d1bff812c83708c4d581f5a593101f80b
Izzy George <george@work.co>
2020-03-25 11:29:16 -0700

    pass props to make autosuggest more generic

--- src/components/Autosuggest/index.js
+++ src/components/Autosuggest/index.js
@@ -1,70 +1,33 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import React from 'react'
+import PropTypes from 'prop-types'
 import Autosuggest from 'react-autosuggest'
-import AutosuggestHighlightMatch from 'autosuggest-highlight/match'
-import AutosuggestHighlightParse from 'autosuggest-highlight/parse'
-import Text from 'components/Text'
-import { TEXT_TYPE } from 'components/Text/constants'
 import styles from './Autosuggest.styles'
 
-const getSuggestionValue = suggestion => suggestion.name
-
-const renderSuggestion = (suggestion, { query }) => {
-  const matches = AutosuggestHighlightMatch(suggestion.name, query)
-  const parts = AutosuggestHighlightParse(suggestion.name, matches)
-  const match = <span>
-    {parts.map((part, index) => {
-      const className = part.highlight ? 'react-autosuggest__suggestion-match' : null
-
-      return (
-        <span className={className} key={index}>
-          {part.text}
-        </span>
-      )
-    })}
-  </span>
-
-  return (
-    <div>
-      <Text as="div" type={TEXT_TYPE.BODY_2}>
-        {match}
-      </Text>
-      <Text as="div" type={TEXT_TYPE.NOTE}>{suggestion.address}</Text>
-    </div>
-  )
-}
-
 class Typeahead extends React.Component {
+  static propTypes = {
+    getSuggestionValue: PropTypes.func.isRequired,
+    onSearch: PropTypes.func.isRequired,
+    suggestions: PropTypes.array.isRequired,
+    renderSuggestion: PropTypes.func.isRequired
+  }
+
   constructor() {
     super()
-
     this.state = {
       isFocused: false,
-      suggestions: [],
       value: '',
     }
   }
 
-  getSuggestions = value => {
-    const inputValue = value.trim().toLowerCase()
-    const inputLength = inputValue.length
-    console.log(!inputLength ? [] : this.props.suggestions.filter(suggest =>
-      suggest.name.toLowerCase().slice(0, inputLength) === inputValue
-    ))
-    return !inputLength ? [] : this.props.suggestions.filter(suggest =>
-      suggest.name.toLowerCase().slice(0, inputLength) === inputValue
-    )
-  }
-
   onChange = (_, { newValue }) => {
-    this.setState({
-      value: newValue
-    })
+    this.setState({ value: newValue })
   }
 
   onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({ suggestions: this.getSuggestions(value) })
+    this.props.onSearch(value)
+    this.setState({ value })
   }
 
   onSuggestionsClearRequested = () => {
@@ -72,13 +35,12 @@ class Typeahead extends React.Component {
   }
 
   toggleFocus = () => {
-    console.log('yuh!')
     this.setState({ isFocused: !this.state.isFocused })
   }
 
   render() {
-    const { isFocused, suggestions, value } = this.state
-    const { label } = this.props
+    const { isFocused, value } = this.state
+    const { label, suggestions, getSuggestionValue, renderSuggestion } = this.props
 
     const inputProps = {
       value,
