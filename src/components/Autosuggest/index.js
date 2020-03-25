f7dcbe6481c551af77133edacf5db08edc8f1e4b
Izzy George <george@work.co>
2020-03-25 13:58:37 -0700

    add submit button with disabled state

--- src/components/Autosuggest/index.js
+++ src/components/Autosuggest/index.js
@@ -1,59 +1,82 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import React from 'react'
-import PropTypes from 'prop-types'
-import Autosuggest from 'react-autosuggest'
-import styles from './Autosuggest.styles'
+import { jsx } from "@emotion/core";
+import React from "react";
+import PropTypes from "prop-types";
+import Autosuggest from "react-autosuggest";
+import styles from "./Autosuggest.styles";
 
 class Typeahead extends React.Component {
   static propTypes = {
     getSuggestionValue: PropTypes.func.isRequired,
     onSearch: PropTypes.func.isRequired,
     suggestions: PropTypes.array.isRequired,
-    renderSuggestion: PropTypes.func.isRequired
-  }
+    renderSuggestion: PropTypes.func.isRequired,
+  };
 
   constructor() {
-    super()
+    super();
     this.state = {
       isFocused: false,
-      value: '',
-    }
+      userInput: "",
+    };
   }
 
   onChange = (_, { newValue }) => {
-    this.setState({ value: newValue })
-  }
+    const isObject = typeof newValue === "object";
+    const value = isObject ? newValue.label : newValue;
+    if (!value) {
+      this.props.onSelect(undefined);
+    }
+
+    this.setState({ userInput: value });
+  };
 
   onSuggestionsFetchRequested = ({ value }) => {
-    this.props.onSearch(value)
-    this.setState({ value })
-  }
+    this.props.onSearch(value);
+    this.setState({ userInput: value });
+  };
 
   onSuggestionsClearRequested = () => {
-    this.setState({ suggestions: [] })
-  }
+    this.setState({ suggestions: [] });
+  };
 
   toggleFocus = () => {
-    this.setState({ isFocused: !this.state.isFocused })
-  }
+    this.setState({ isFocused: !this.state.isFocused });
+  };
+
+  onSuggestionSelect = (e, { suggestion }) => {
+    e.preventDefault();
+    this.props.onSelect(suggestion.id);
+  };
 
   render() {
-    const { isFocused, value } = this.state
-    const { label, suggestions, getSuggestionValue, renderSuggestion } = this.props
+    const { isFocused, userInput } = this.state;
+    const {
+      label,
+      suggestions,
+      getSuggestionValue,
+      renderSuggestion,
+    } = this.props;
 
     const inputProps = {
-      value,
+      value: userInput,
       onBlur: this.toggleFocus,
       onChange: this.onChange,
       onFocus: this.toggleFocus,
-    }
+    };
 
     return (
       <div css={[styles.root, isFocused && styles.active]}>
         <label css={styles.container}>
           {label && (
-            <div css={[styles.label, (isFocused || value) && styles.activeLabel]}>{label}</div>
+            <div
+              css={[
+                styles.label,
+                (isFocused || userInput) && styles.activeLabel,
+              ]}
+            >
+              {label}
+            </div>
           )}
         </label>
         <Autosuggest
@@ -61,12 +84,13 @@ class Typeahead extends React.Component {
           onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
           onSuggestionsClearRequested={this.onSuggestionsClearRequested}
           getSuggestionValue={getSuggestionValue}
+          onSuggestionSelected={this.onSuggestionSelect}
           renderSuggestion={renderSuggestion}
           inputProps={inputProps}
         />
       </div>
-    )
+    );
   }
 }
 
-export default Typeahead
+export default Typeahead;
--- src/pages/Request/index.js
+++ src/pages/Request/index.js
@@ -1,49 +1,57 @@
 /** @jsx jsx */
-import React from 'react'
-import { jsx } from '@emotion/core'
+import React from "react";
+import { jsx } from "@emotion/core";
 import * as tools from "../../functions";
 import * as hospital_index from "../../data/hospital_index";
-import AutosuggestHighlightMatch from 'autosuggest-highlight/match'
-import AutosuggestHighlightParse from 'autosuggest-highlight/parse'
-import Text from 'components/Text'
-import { TEXT_TYPE } from 'components/Text/constants'
+import AutosuggestHighlightMatch from "autosuggest-highlight/match";
+import AutosuggestHighlightParse from "autosuggest-highlight/parse";
+import Text from "components/Text";
+import { TEXT_TYPE } from "components/Text/constants";
 import { withRouter } from "react-router-dom";
 import { Form } from "react-bootstrap";
-import Autosuggest from 'components/Autosuggest'
+import Autosuggest from "components/Autosuggest";
 import Box from "components/Box";
-import { IconButton } from 'components/Button'
-import { ReactComponent as Back } from 'static/icons/back-circle.svg'
+import { IconButton, PrimaryButton } from "components/Button";
+import { ReactComponent as Back } from "static/icons/back-circle.svg";
 
-import styles from './Request.styles.js'
+import styles from "./Request.styles.js";
 
 const renderSuggestion = ({ hospital }, { query }) => {
-  const nameMatches = AutosuggestHighlightMatch(hospital.name, query)
-  const nameParts = AutosuggestHighlightParse(hospital.name, nameMatches)
-  const nameMatch = <span>
-    {nameParts.map((part, index) => {
-      const className = part.highlight ? 'react-autosuggest__suggestion-match' : null
+  const nameMatches = AutosuggestHighlightMatch(hospital.name, query);
+  const nameParts = AutosuggestHighlightParse(hospital.name, nameMatches);
+  const nameMatch = (
+    <span>
+      {nameParts.map((part, index) => {
+        const className = part.highlight
+          ? "react-autosuggest__suggestion-match"
+          : null;
 
-      return (
-        <span className={className} key={index}>
-          {part.text}
-        </span>
-      )
-    })}
-  </span>
+        return (
+          <span className={className} key={index}>
+            {part.text}
+          </span>
+        );
+      })}
+    </span>
+  );
 
-  const cityMatches = AutosuggestHighlightMatch(hospital.name, query)
-  const cityParts = AutosuggestHighlightParse(hospital.city, cityMatches)
-  const cityMatch = <span>
-    {cityParts.map((part, index) => {
-      const className = part.highlight ? 'react-autosuggest__suggestion-match' : null
+  const cityMatches = AutosuggestHighlightMatch(hospital.name, query);
+  const cityParts = AutosuggestHighlightParse(hospital.city, cityMatches);
+  const cityMatch = (
+    <span>
+      {cityParts.map((part, index) => {
+        const className = part.highlight
+          ? "react-autosuggest__suggestion-match"
+          : null;
 
-      return (
-        <span className={className} key={index}>
-          {part.text}
-        </span>
-      )
-    })}
-  </span>
+        return (
+          <span className={className} key={index}>
+            {part.text}
+          </span>
+        );
+      })}
+    </span>
+  );
 
   return (
     <div>
@@ -54,25 +62,29 @@ const renderSuggestion = ({ hospital }, { query }) => {
         {cityMatch}, {hospital.state}
       </Text>
     </div>
-  )
-}
+  );
+};
 
-const getHospitalName = suggestion => suggestion.hospital.name
+const getHospitalName = ({ hospital, id }) => ({
+  label: hospital.name,
+  value: id,
+});
 
 class Request extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       results: [],
-      facilities: [],
-      selectedResults: ""
+      selectedResult: "",
     };
-    this.handleChangeDonate = this.handleChangeDonate.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSelectHospital = this.handleSelectHospital.bind(this);
     this.handleRedirect = this.handleRedirect.bind(this);
   }
+
+  handleSelectHospital = (value) => {
+    this.setState({ selectedResult: value });
+  };
 
   handleChange(value) {
     if (value.length > 1) {
@@ -82,43 +94,31 @@ class Request extends React.Component {
     } else {
       this.setState({
         results: [],
-        selectedResult: ""
+        selectedResult: "",
       });
     }
   }
 
-  handleSelectHospital(id) {
-    this.setState({ selectedResult: id });
-  }
-
-  handleSubmit(event) {
-    // this prevents the page from reloading when form is submitted
-    if (event) {
-      event.preventDefault();
-    }
-    if (this.state.selectedResult !== "") {
-      this.handleRedirect();
-    }
-  }
-
   handleRedirect() {
-    if (this.state.selectedResult !== "") {
-      if (this.props.backend.authLoaded && this.props.backend.isLoggedIn()) {
-        this.props.backend
-          .dropSiteExists(this.state.selectedResult)
-          .then(exists => {
-            if (exists) {
-              let url = "/dropsite/" + this.state.selectedResult + "/admin";
-              this.props.history.push(url);
-            } else {
-              let url = "/dropsite/new/admin/" + this.state.selectedResult;
-              this.props.history.push(url);
-            }
-          });
-      } else {
-        let url = "/signup/" + this.state.selectedResult;
-        this.props.history.push(url);
-      }
+    if (!this.state.selectedResult) {
+      return;
+    }
+
+    if (this.props.backend.authLoaded && this.props.backend.isLoggedIn()) {
+      this.props.backend
+        .dropSiteExists(this.state.selectedResult)
+        .then((exists) => {
+          if (exists) {
+            let url = "/dropsite/" + this.state.selectedResult + "/admin";
+            this.props.history.push(url);
+          } else {
+            let url = "/dropsite/new/admin/" + this.state.selectedResult;
+            this.props.history.push(url);
+          }
+        });
+    } else {
+      let url = "/signup/" + this.state.selectedResult;
+      this.props.history.push(url);
     }
   }
 
@@ -126,33 +126,37 @@ class Request extends React.Component {
     event.preventDefault();
   }
 
-  componentDidMount() {
-    this.props.backend.listDropSites().then(data => {
-      this.setState({ facilities: data });
-    });
-  }
-
-  // DONATE SUPPLIES DROPDOWN HANDLER
-  handleChangeDonate(event) {
-    let url = "/dropsite/" + event.target.value;
-    this.props.history.push(url);
-  }
-
   render() {
     return (
       <Box>
-        <IconButton css={styles.back}>
-          <Back />
-        </IconButton>
-        <Text as="h3" type={TEXT_TYPE.HEADER_3} css={styles.title}>Find your healthcare facility</Text>
-        <Text as="p" type={TEXT_TYPE.BODY_2}>I'm a healthcare professional working at:</Text>
-        <Autosuggest
-          label="City or healthcare facility"
-          suggestions={this.state.results}
-          onSearch={this.handleChange}
-          getSuggestionValue={getHospitalName}
-          renderSuggestion={renderSuggestion}
-        />
+        <form onSubmit={this.handleRedirect} css={styles.root}>
+          <div css={styles.form}>
+            <IconButton css={styles.back}>
+              <Back />
+            </IconButton>
+            <Text as="h3" type={TEXT_TYPE.HEADER_3} css={styles.title}>
+              Find your healthcare facility
+            </Text>
+            <Text as="p" type={TEXT_TYPE.BODY_2}>
+              I'm a healthcare professional working at:
+            </Text>
+            <Autosuggest
+              label="City or healthcare facility"
+              suggestions={this.state.results}
+              onSearch={this.handleChange}
+              getSuggestionValue={getHospitalName}
+              renderSuggestion={renderSuggestion}
+              onSelect={this.handleSelectHospital}
+            />
+          </div>
+          <PrimaryButton
+            onClick={this.handleRedirect}
+            disabled={!this.state.selectedResult}
+            css={styles.button}
+          >
+            <Text type={TEXT_TYPE.BODY_1}>Next</Text>
+          </PrimaryButton>
+        </form>
       </Box>
     );
   }
