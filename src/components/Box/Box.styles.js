fe9a5e526cf4c7ad3d9d4ba31beb63608b902e4b
Izzy George <george@work.co>
2020-03-25 11:29:34 -0700

    create page layout for padding and add request page

---
+++ src/components/Box/Box.styles.js
@@ -0,0 +1,13 @@
+import { css } from '@emotion/core'
+import { Color, Space } from 'lib/theme'
+
+const styles = {
+  root: css({
+    color: Color.GRAY,
+    width: '100%',
+    margin: 'auto',
+    padding: `${Space.S30}px ${Space.S40}px`
+  }),
+}
+
+export default styles
---
+++ src/pages/Request/Request.styles.js
@@ -0,0 +1,13 @@
+import { css } from '@emotion/core'
+import { Space } from 'lib/theme'
+
+const styles = {
+  back: css({
+    marginBottom: Space.S30,
+  }),
+  title: css({
+    marginBottom: Space.S15
+  })
+}
+
+export default styles
