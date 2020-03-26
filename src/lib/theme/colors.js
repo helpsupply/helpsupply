07c93a2db92317762ba1f4dafe95ccfbc9a2596b
Izzy George <george@work.co>
2020-03-26 14:14:18 -0700

    create checkbox and add to drop site form

--- src/lib/theme/colors.js
+++ src/lib/theme/colors.js
@@ -3,6 +3,7 @@ export const Color = {
   CORAL_30: '#FFCECF', // 30% of Color.CORAL
   GRAY: '#484848',
   GRAY_10: '#ECECEC', // 10% opacity of Color.GRAY
+  GRAY_30: '#C8C8C8', // 30% opacity
   GRAY_50: '#9F9F9F', // 50% opacity
   GRAY_75: '#737373', // 75% opacity
   GRAY_MEDIUM: '#C4C4C4',
