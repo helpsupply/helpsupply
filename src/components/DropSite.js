fbfae142c06cdf6dcb18f6000640f725d48b7dd6
Pujit Mehrotra <pmehrotra2@babson.edu>
2020-03-26 14:42:27 -0400

    Fix #64 by adding null check to donationFormElement
    Link to issue: https://github.com/newhouseb/helpsupply/issues/64

--- src/components/DropSite.js
+++ src/components/DropSite.js
@@ -1,61 +1,63 @@
-import React from "react";
-import * as hospital_index from "../data/hospital_index";
-import { withRouter, Link } from "react-router-dom";
-import DropSiteNeedGroup from "./DropSiteNeedGroup";
-import NewSupplyForm from "./NewSupplyForm";
-import HelpFooter from "./HelpFooter";
-import CartBanner from "./CartBanner";
+import React from 'react'
+import * as hospital_index from '../data/hospital_index'
+import { withRouter, Link } from 'react-router-dom'
+import DropSiteNeedGroup from './DropSiteNeedGroup'
+import NewSupplyForm from './NewSupplyForm'
+import HelpFooter from './HelpFooter'
+import CartBanner from './CartBanner'
 
 class DropSite extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      dropSiteId: "",
-      dropSiteName: "",
-      dropSiteAddress: "",
-      dropSiteZip: "",
-      dropSiteDescription: "",
+      dropSiteId: '',
+      dropSiteName: '',
+      dropSiteAddress: '',
+      dropSiteZip: '',
+      dropSiteDescription: '',
       needs: [],
       supply: [],
       cart: [],
-      alertClicked: false
-    };
-    this.handleNewSupply = this.handleNewSupply.bind(this);
-    this.handleAddToCart = this.handleAddToCart.bind(this);
-    this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
-    this.handleCartScroll = this.handleCartScroll.bind(this);
+      alertClicked: false,
+    }
+    this.handleNewSupply = this.handleNewSupply.bind(this)
+    this.handleAddToCart = this.handleAddToCart.bind(this)
+    this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this)
+    this.handleCartScroll = this.handleCartScroll.bind(this)
   }
 
   handleCartScroll() {
-    var element = document.getElementById("donationForm");
-    element.scrollIntoView({ behavior: "smooth" });
+    let element = document.getElementById('donationForm')
+    if (Boolean(element)) {
+      element.scrollIntoView({ behavior: 'smooth' })
+    }
     this.setState({
-      alertClicked: true
-    });
+      alertClicked: true,
+    })
   }
 
   handleNewSupply(supplyObj) {
-    let oldList = this.state.supply;
-    oldList.push(supplyObj);
+    let oldList = this.state.supply
+    oldList.push(supplyObj)
     this.setState({
-      supply: oldList
-    });
+      supply: oldList,
+    })
   }
 
   handleAddToCart(itemToAdd) {
-    let oldList = this.state.cart;
-    oldList.push(itemToAdd);
+    let oldList = this.state.cart
+    oldList.push(itemToAdd)
     this.setState({
-      cart: oldList
-    });
+      cart: oldList,
+    })
   }
 
   handleRemoveFromCart(i) {
-    let oldList = this.state.cart;
-    oldList.splice(i, 1);
+    let oldList = this.state.cart
+    oldList.splice(i, 1)
     this.setState({
-      cart: oldList
-    });
+      cart: oldList,
+    })
   }
 
   handleChange(event) {}
@@ -63,27 +65,27 @@ class DropSite extends React.Component {
   handleSubmit(event) {}
 
   componentDidMount() {
-    this.props.backend.getRequests(this.props.match.params.id).then(data => {
+    this.props.backend.getRequests(this.props.match.params.id).then((data) => {
       this.setState(
         {
-          needs: data
+          needs: data,
         },
         () => {
           // console.log(this.state);
         }
-      );
-    });
-    this.props.backend.listSupply(this.props.match.params.id).then(data => {
+      )
+    })
+    this.props.backend.listSupply(this.props.match.params.id).then((data) => {
       this.setState(
         {
-          supply: data
+          supply: data,
         },
         () => {
           // console.log(this.state);
         }
-      );
-    });
-    this.props.backend.getDropSites(this.props.match.params.id).then(data => {
+      )
+    })
+    this.props.backend.getDropSites(this.props.match.params.id).then((data) => {
       this.setState(
         {
           dropSiteId: data.location_id,
@@ -92,31 +94,31 @@ class DropSite extends React.Component {
           dropSiteZip: data.dropSiteZip,
           dropSiteDescription: data.dropSiteDescription,
           dropSiteHospital: data.dropSiteHospital,
-          dropSitePhone: data.dropSitePhone
+          dropSitePhone: data.dropSitePhone,
         },
         () => {
           //console.log(this.state);
         }
-      );
-    });
+      )
+    })
   }
 
   componentDidUpdate() {}
 
   render() {
     // this code is for the future when some dropsites may not map to an exisitng hospital in the hospital_index
-    let hospital = hospital_index.index.id_index[this.props.match.params.id];
-    let hospitalText = "";
-    if (typeof hospital === "undefined") {
+    let hospital = hospital_index.index.id_index[this.props.match.params.id]
+    let hospitalText = ''
+    if (typeof hospital === 'undefined') {
       hospitalText = (
         <div className="servingText">
           (serving {this.state.dropSiteHospital})
         </div>
-      );
+      )
     } else {
       hospitalText = (
         <div className="servingText">(serving {hospital.name})</div>
-      );
+      )
     }
 
     return (
@@ -125,7 +127,7 @@ class DropSite extends React.Component {
           <span className="navbar-brand mb-0 h1" id="hospitalname">
             <div className="dropSiteIdText">
               <b>Drop-Off Location: {this.state.dropSiteId}</b>
-            </div>{" "}
+            </div>{' '}
             {hospitalText}
           </span>
           <a href="/" className="navbar-brand mb-0 h1 logored">
@@ -135,9 +137,8 @@ class DropSite extends React.Component {
         <div className="content">
           <div className="panelFull">
             <Link
-              to={"/signup/" + this.state.dropSiteId}
-              className="manageDropSiteLink"
-            >
+              to={'/signup/' + this.state.dropSiteId}
+              className="manageDropSiteLink">
               Manage This Drop-off Location
             </Link>
             <div className="hospitalNeedsTopBar">
@@ -214,7 +215,7 @@ class DropSite extends React.Component {
                       <th>{supply.requestTitle}</th>
                       <th>{supply.supplyQuantity}</th>
                     </tr>
-                  );
+                  )
                 })}
               </tbody>
             </table>
@@ -228,8 +229,8 @@ class DropSite extends React.Component {
           />
         )}
       </div>
-    );
+    )
   }
 }
 
-export default withRouter(DropSite);
+export default withRouter(DropSite)
