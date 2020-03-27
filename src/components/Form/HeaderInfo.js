e96e64646f76c14c77365295315c42bdab0785ac
Wes Souza <souza@work.co>
2020-03-27 15:29:53 -0400

    refactor(react-hooks): refactor components/Form

--- src/components/Form/HeaderInfo.js
+++ src/components/Form/HeaderInfo.js
@@ -1,5 +1,4 @@
 /** @jsx jsx */
-import React from 'react';
 import { jsx } from '@emotion/core';
 import PropTypes from 'prop-types';
 import Text from 'components/Text';
@@ -7,38 +6,31 @@ import { TEXT_TYPE } from 'components/Text/constants';
 
 import styles from './Form.styles.js';
 
-class InfoHeader extends React.Component {
-  static propTypes = {
-    description: PropTypes.string,
-    title: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {
-      as = 'h3',
-      description,
-      title,
-      type = TEXT_TYPE.HEADER_3,
-    } = this.props;
-    return (
-      <div>
-        {title && (
-          <Text as={as} type={type} css={styles.title}>
-            {title}
-          </Text>
-        )}
-        {description && (
-          <Text as="p" type={TEXT_TYPE.BODY_2}>
-            {description}
-          </Text>
-        )}
-      </div>
-    );
-  }
+function InfoHeader({
+  as = 'h3',
+  description,
+  title,
+  type = TEXT_TYPE.HEADER_3,
+}) {
+  return (
+    <div>
+      {title && (
+        <Text as={as} type={type} css={styles.title}>
+          {title}
+        </Text>
+      )}
+      {description && (
+        <Text as="p" type={TEXT_TYPE.BODY_2}>
+          {description}
+        </Text>
+      )}
+    </div>
+  );
 }
+
+InfoHeader.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default InfoHeader;
