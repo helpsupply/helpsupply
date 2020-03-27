813bb55600f06c4e33930417bb20dd3f58fed930
Wes Souza <souza@work.co>
2020-03-27 16:17:51 -0400

    refactor(react-hooks): refactor containers/FacilityForm

--- src/containers/FacilityForm.js
+++ src/containers/FacilityForm.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { jsx } from '@emotion/core';
 import Form from 'components/Form';
 import InputText from 'components/InputText';
@@ -9,123 +9,118 @@ import Note from 'components/Note';
 import states from 'data/states';
 import FacilityConfirmation from 'components/FacilityConfirmation';
 
-class FacilityForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropSiteId: '',
-      fields: {
-        dropSiteFacilityName: '',
-        dropSiteZip: '',
-        dropSiteAddress: '',
-        dropSiteCity: '',
-        dropSiteState: '',
-        dropSiteUrl: '',
-      },
-    };
-  }
+function FacilityForm({ backend, history }) {
+  const [dropSiteId, setDropSiteId] = useState('');
+  const [fields, setFields] = useState({
+    dropSiteFacilityName: '',
+    dropSiteZip: '',
+    dropSiteAddress: '',
+    dropSiteCity: '',
+    dropSiteState: '',
+    dropSiteUrl: '',
+  });
 
-  componentWillUpdate(_, nextState) {
-    if (!this.state.dropSiteId && nextState.dropSiteId) {
-      this.props.backend.getDropSites(nextState.dropSiteId).then((data) => {
-        data && this.setState({ fields: data });
-      });
-    }
-  }
-
-  handleFieldChange = (field) => (value) => {
-    this.setState({ fields: { ...this.state.fields, [field]: value } });
-  };
-
-  handleSubmit = () => {
-    this.props.backend.addNewDropSite(this.state.fields).then((data) => {
+  useEffect(() => {
+    backend.getDropSites(dropSiteId).then((data) => {
       if (!data) {
         return;
       }
-      this.setState({ dropSiteId: data });
+      setFields(data);
     });
-  };
+  }, [backend, dropSiteId]);
 
-  render() {
-    const { dropSiteUrl, ...requiredFields } = this.state.fields;
+  const handleFieldChange = useCallback(
+    (field) => (value) => {
+      setFields((fields) => ({
+        ...fields,
+        [field]: value,
+      }));
+    },
+    [],
+  );
 
-    if (this.state.dropSiteId) {
-      const {
-        dropSiteFacilityName,
-        dropSiteAddress,
-        dropSiteCity,
-        dropSiteState,
-        dropSiteZip,
-      } = this.state.fields;
-      return (
-        <Form
-          onSubmit={() =>
-            this.props.history.push(`/signup/${this.state.dropSiteId}`)
-          }
-        >
-          <FacilityConfirmation
-            name={dropSiteFacilityName}
-            address={[
-              dropSiteAddress,
-              dropSiteCity,
-              dropSiteState,
-              dropSiteZip,
-            ].join(', ')}
-          />
-        </Form>
-      );
-    }
+  const handleSubmit = useCallback(() => {
+    backend.addNewDropSite(fields).then((data) => {
+      if (!data) {
+        return;
+      }
+      setDropSiteId(data);
+    });
+  }, [backend, fields]);
 
+  const { dropSiteUrl, ...requiredFields } = fields;
+
+  const {
+    dropSiteFacilityName,
+    dropSiteAddress,
+    dropSiteCity,
+    dropSiteState,
+    dropSiteZip,
+  } = fields;
+
+  if (dropSiteId) {
     return (
-      <Form
-        buttonLabel="Submit"
-        onSubmit={this.handleSubmit}
-        title="Add a new facility"
-        disabled={
-          !Object.keys(requiredFields).every((key) => !!this.state.fields[key])
-        }
-        description="Enter some information about your facility."
-      >
-        <InputText
-          label="Name of the facility"
-          value={this.state.dropSiteFacilityName}
-          customOnChange={this.handleFieldChange('dropSiteFacilityName')}
+      <Form onSubmit={() => history.push(`/signup/${dropSiteId}`)}>
+        <FacilityConfirmation
+          name={dropSiteFacilityName}
+          address={[
+            dropSiteAddress,
+            dropSiteCity,
+            dropSiteState,
+            dropSiteZip,
+          ].join(', ')}
         />
-        <InputText
-          label="Zip code"
-          value={this.state.dropSiteZip}
-          customOnChange={this.handleFieldChange('dropSiteZip')}
-        />
-        <div css={{ display: 'flex', '> *': { width: '50%' } }}>
-          <InputText
-            label="City"
-            value={this.state.dropSiteCity}
-            customOnChange={this.handleFieldChange('dropSiteCity')}
-          />
-          <InputDropdown
-            placeholder="State"
-            value={this.state.dropSiteState}
-            options={states}
-            customOnChange={this.handleFieldChange('dropSiteState')}
-          />
-        </div>
-        <InputText
-          label="Full Street Address"
-          value={this.state.dropSiteAddress}
-          customOnChange={this.handleFieldChange('dropSiteAddress')}
-        />
-        <InputText
-          label="Website URL (optional)"
-          value={this.state.dropSiteUrl}
-          customOnChange={this.handleFieldChange('dropSiteUrl')}
-        />
-        <Note>
-          If you’re working from a temporary facility, email us at{' '}
-          <Anchor href="mailto:help@help.supply">help@help.supply</Anchor>
-        </Note>
       </Form>
     );
   }
+
+  return (
+    <Form
+      buttonLabel="Submit"
+      onSubmit={handleSubmit}
+      title="Add a new facility"
+      disabled={!Object.keys(requiredFields).every((key) => !!fields[key])}
+      description="Enter some information about your facility."
+    >
+      <InputText
+        label="Name of the facility"
+        value={dropSiteFacilityName}
+        customOnChange={handleFieldChange('dropSiteFacilityName')}
+      />
+      <InputText
+        label="Zip code"
+        value={dropSiteZip}
+        customOnChange={handleFieldChange('dropSiteZip')}
+      />
+      <div css={{ display: 'flex', '> *': { width: '50%' } }}>
+        <InputText
+          label="City"
+          value={dropSiteCity}
+          customOnChange={handleFieldChange('dropSiteCity')}
+        />
+        <InputDropdown
+          placeholder="State"
+          value={dropSiteState}
+          options={states}
+          customOnChange={handleFieldChange('dropSiteState')}
+        />
+      </div>
+      <InputText
+        label="Full Street Address"
+        value={dropSiteAddress}
+        customOnChange={handleFieldChange('dropSiteAddress')}
+      />
+      <InputText
+        label="Website URL (optional)"
+        value={dropSiteUrl}
+        customOnChange={handleFieldChange('dropSiteUrl')}
+      />
+      <Note>
+        If you’re working from a temporary facility, email us at{' '}
+        <Anchor href="mailto:help@help.supply">help@help.supply</Anchor>
+      </Note>
+    </Form>
+  );
 }
 
 export default FacilityForm;
