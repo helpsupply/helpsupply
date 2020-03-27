8f85ce027021d18bbac9a378a329edb7a227bed1
tricia-work <leach@work.co>
2020-03-26 20:51:51 -0700

    feat(start page): add logo icon and LargeHeader component

---
+++ src/components/Header/LargeHeader.styles.js
@@ -0,0 +1,15 @@
+import { css } from '@emotion/core'
+import { Color, Space } from 'lib/theme'
+
+const styles = {
+  root: css({
+    alignItems: 'flex-end',
+    backgroundColor: `${Color.CORAL}`,
+    display: 'flex',
+    minHeight: '276px',
+    padding: `${Space.S40}px`,
+    width: '100%',
+  }),
+}
+
+export default styles
