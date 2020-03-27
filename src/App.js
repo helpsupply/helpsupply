9e519751b54609e17bf49df1832fc58d722ad1e4
davydka <davydka@gmail.com>
2020-03-27 10:44:43 -0700

    fix: remove duplicate route (#86)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { Global } from '@emotion/core'
-import { styles } from './App.styles'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HCPSignupFinish from './components/HCPSignupFinish'
-import DropSiteAdmin from './components/DropSiteAdmin'
-import DropSite from './components/DropSite'
-import PendingDomains from './components/PendingDomains'
-import NoMatch from './components/NoMatch'
-import Login from './components/Login'
-import Logout from './components/Logout'
-import Profile from './components/Profile'
-import StyleGuide from './components/StyleGuide'
+import React from 'react';
+import { Global } from '@emotion/core';
+import { styles } from './App.styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HCPSignupFinish from './components/HCPSignupFinish';
+import DropSiteAdmin from './components/DropSiteAdmin';
+import DropSite from './components/DropSite';
+import PendingDomains from './components/PendingDomains';
+import NoMatch from './components/NoMatch';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import StyleGuide from './components/StyleGuide';
 
-import NewSupplyRequest from 'pages/supplies_new'
-import AdminDropSite from 'pages/dropsite_admin'
-import NewFacility from 'pages/facility_new'
-import NewDropSite from 'pages/dropsite_new'
-import EntryPortal from 'pages/entry'
-import Request from 'pages/request'
-import SignUp from 'pages/signup'
+import NewSupplyRequest from 'pages/supplies_new';
+import AdminDropSite from 'pages/dropsite_admin';
+import NewFacility from 'pages/facility_new';
+import NewDropSite from 'pages/dropsite_new';
+import EntryPortal from 'pages/entry';
+import Request from 'pages/request';
+import SignUp from 'pages/signup';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       user: {},
       userData: {},
-    }
+    };
   }
 
   componentDidMount() {}
@@ -73,9 +73,6 @@ class App extends React.Component {
               <Route exact path="/">
                 <EntryPortal backend={this.props.backend} />
               </Route>
-              <Route path="/signup/:dropsite?">
-                <SignUp backend={this.props.backend} />
-              </Route>
               <Route path="/request">
                 <Request backend={this.props.backend} />
               </Route>
@@ -93,8 +90,8 @@ class App extends React.Component {
           </div>
         </Router>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
