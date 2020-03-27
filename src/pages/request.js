4d79339ca8dfde165ea2c910ac0a66c5c7d32218
Wes Souza <souza@work.co>
2020-03-27 16:46:45 -0400

    refactor(react-hooks): refactor pages/request

--- src/pages/request.js
+++ src/pages/request.js
@@ -1,19 +1,16 @@
 /** @jsx jsx */
-import React from 'react';
 import { jsx } from '@emotion/core';
 import { withRouter } from 'react-router-dom';
 
 import Page from 'components/layouts/Page';
 import FindFacility from 'containers/FindFacility';
 
-class Request extends React.Component {
-  render() {
-    return (
-      <Page>
-        <FindFacility {...this.props} />
-      </Page>
-    );
-  }
+function Request(props) {
+  return (
+    <Page>
+      <FindFacility {...props} />
+    </Page>
+  );
 }
 
 export default withRouter(Request);
