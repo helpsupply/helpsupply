bd5e839563890b59dde4194316bd084570bd798e
Wes Souza <souza@work.co>
2020-03-27 16:46:41 -0400

    refactor(react-hooks): refactor pages/facility_new

--- src/pages/facility_new.js
+++ src/pages/facility_new.js
@@ -1,19 +1,16 @@
 /** @jsx jsx */
-import React from 'react';
 import { jsx } from '@emotion/core';
 import { withRouter } from 'react-router-dom';
 
 import Page from 'components/layouts/Page';
 import FacilityForm from 'containers/FacilityForm';
 
-class NewFacility extends React.Component {
-  render() {
-    return (
-      <Page>
-        <FacilityForm {...this.props} />
-      </Page>
-    );
-  }
+function NewFacility(props) {
+  return (
+    <Page>
+      <FacilityForm {...props} />
+    </Page>
+  );
 }
 
 export default withRouter(NewFacility);
