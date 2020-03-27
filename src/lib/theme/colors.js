55d248848dd9e15d8a7836e35e4fe8bacb1bb06f
tricia-work <leach@work.co>
2020-03-26 23:34:42 -0700

    feat(start page): add ListLink component and arrow icon

--- src/lib/theme/colors.js
+++ src/lib/theme/colors.js
@@ -4,6 +4,7 @@ export const Color = {
   GREEN: '#3BD089',
   GRAY: '#484848',
   GRAY_10: '#ECECEC', // 10% opacity of Color.GRAY
+  GRAY_25: '#D1D1D1', // 25% opacity
   GRAY_30: '#C8C8C8', // 30% opacity
   GRAY_50: '#9F9F9F', // 50% opacity
   GRAY_75: '#737373', // 75% opacity
