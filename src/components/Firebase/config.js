4fd32d62b6ba60a1222944fa172b823f1f6ba94d
Josh Rounsville <joshuarounsville@gmail.com>
2020-03-27 22:06:51 -0700

    feat(builds): add deployment scripts

--- src/components/Firebase/config.js
+++ src/components/Firebase/config.js
@@ -1,17 +1,6 @@
 let config = null;
 
-if (window.location.hostname === 'localhost') {
-  config = {
-    apiKey: 'AIzaSyAXmpRfgvne_w2apeWb3Q8wSNUqNey3-Mg',
-    authDomain: 'help-supply-staging.firebaseapp.com',
-    databaseURL: 'https://help-supply-staging.firebaseio.com',
-    projectId: 'help-supply-staging',
-    storageBucket: 'help-supply-staging.appspot.com',
-    messagingSenderId: '395198068697',
-    appId: '1:395198068697:web:3a7975e877fbc98c190bc8',
-    measurementId: 'G-5SYKT9JHQF',
-  };
-} else {
+if (process.env.REACT_APP_HOST_ENV === 'production') {
   config = {
     apiKey: 'AIzaSyBtHRUSPc0e4rF057rotTeVpeZ3lDWoQTE',
     authDomain: 'hospitalcommunity.firebaseapp.com',
@@ -21,6 +10,17 @@ if (window.location.hostname === 'localhost') {
     messagingSenderId: '726825294724',
     appId: '1:726825294724:web:f3dc998705fbe6c8b09bd1',
     measurementId: 'G-987WXEFK5D',
+  };
+} else {
+  config = {
+    apiKey: 'AIzaSyAXmpRfgvne_w2apeWb3Q8wSNUqNey3-Mg',
+    authDomain: 'help-supply-staging.firebaseapp.com',
+    databaseURL: 'https://help-supply-staging.firebaseio.com',
+    projectId: 'help-supply-staging',
+    storageBucket: 'help-supply-staging.appspot.com',
+    messagingSenderId: '395198068697',
+    appId: '1:395198068697:web:3a7975e877fbc98c190bc8',
+    measurementId: 'G-5SYKT9JHQF',
   };
 }
 
