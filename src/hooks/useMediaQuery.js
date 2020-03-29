4c7189680ae4e363129c2d5d1d6a37aee605ed7c
Wes Souza <souza@work.co>
2020-03-29 12:40:51 -0400

    feat: add useMediaQuery hook

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { BreakpointsAll } from 'constants/Breakpoints';
+
+export function useMediaQuery() {
+  const mediaQueryLists = useRef(
+    BreakpointsAll.map((minWidth) =>
+      window.matchMedia(`(min-width: ${minWidth}px)`),
+    ),
+  );
+  const [matches, setMatches] = useState(
+    mediaQueryLists.current.map((mediaQueryList) => mediaQueryList.matches),
+  );
+
+  const handleMatchUpdate = useCallback(() => {
+    setMatches(
+      mediaQueryLists.current.map((mediaQueryList) => mediaQueryList.matches),
+    );
+  }, []);
+
+  useEffect(() => {
+    const cachedMediaQueryLists = mediaQueryLists.current;
+    cachedMediaQueryLists.forEach((mediaQueryList) => {
+      mediaQueryList.addListener(handleMatchUpdate);
+    });
+    return () => {
+      cachedMediaQueryLists.forEach((mediaQueryList) => {
+        mediaQueryList.removeListener(handleMatchUpdate);
+      });
+    };
+  }, [handleMatchUpdate]);
+
+  const matchesBreakpoint = useCallback(
+    (breakpoint) => {
+      const breakpointIndex = BreakpointsAll.indexOf(breakpoint);
+      return matches[breakpointIndex] ?? false;
+    },
+    [matches],
+  );
+
+  return { matchesBreakpoint };
+}
