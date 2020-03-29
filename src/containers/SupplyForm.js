074eae325e1ab6e2208586d8f969adc492cde09e
davydka <davydka@gmail.com>
2020-03-28 22:52:16 -0700

    feat: adding new fields to the supply form

--- src/containers/SupplyForm.js
+++ src/containers/SupplyForm.js
@@ -2,17 +2,23 @@
 import { useState, useCallback } from 'react';
 import { jsx } from '@emotion/core';
 import Form from 'components/Form';
-import InputText from 'components/InputText';
+import InputCheckbox from 'components/Checkbox';
 import InputDropdown from 'components/InputDropdown';
+import InputText from 'components/InputText';
 import Note from 'components/Note';
+import Text from 'components/Text';
+import { TEXT_TYPE } from 'components/Text/constants';
+import TextArea from 'components/TextArea';
 import { useTranslation } from 'react-i18next';
 
 function SupplyForm({ onSubmit }) {
   const { t } = useTranslation();
   const [fields, setFields] = useState({
+    detailedRequirements: undefined,
     type: undefined,
     kind: undefined,
-    quantity: '',
+    quantity: undefined,
+    requestWillingToPay: false,
   });
 
   const handleFieldChange = useCallback(
@@ -26,7 +32,7 @@ function SupplyForm({ onSubmit }) {
     onSubmit(fields);
   }, [fields, onSubmit]);
 
-  const { type, kind, quantity } = fields;
+  const { detailedRequirements, type, kind, quantity } = fields;
 
   return (
     <Form
@@ -55,6 +61,26 @@ function SupplyForm({ onSubmit }) {
         value={quantity}
         customOnChange={handleFieldChange('quantity')}
       />
+      <Note>{t('request.supplyForm.disclaimer')}</Note>
+
+      <Text as="p" type={TEXT_TYPE.BODY_2}>
+        {t('request.supplyForm.detailedRequirements.note')}
+      </Text>
+
+      <TextArea
+        customOnChange={handleFieldChange('detailedRequirements')}
+        label={t('request.supplyForm.detailedRequirements.label')}
+        name="detailedRequirements"
+        value={detailedRequirements}
+      />
+
+      <InputCheckbox
+        customOnChange={handleFieldChange('requestWillingToPay')}
+        label={t('request.supplyForm.facilityWillPayLargeVolumes.label')}
+        name="facilityWillPayLargeVolumes"
+        value="1"
+      />
+
       <Note>{t('request.supplyForm.disclaimer')}</Note>
     </Form>
   );
