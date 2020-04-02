ded5955467f0308cdc2a6959d150f5fefd752804
Josh Rounsville <joshuarounsville@gmail.com>
2020-04-02 16:09:42 -0700

    feat(state): persist data to localstorage

---
+++ src/state/persistState.js
@@ -0,0 +1,18 @@
+const stateKey = 'state';
+
+export const persistState = (state, action) => {
+  const actionDataKey = Object.keys(action).find((key) => key !== 'type');
+  const actionData = action[actionDataKey];
+
+  const newState = { ...state, [actionDataKey]: { ...actionData } };
+  const stringifiedState = JSON.stringify(newState);
+
+  localStorage.setItem(stateKey, stringifiedState);
+};
+
+export const getPersistedState = () => {
+  const stateString = localStorage.getItem(stateKey);
+  return JSON.parse(stateString);
+};
+
+export const clearPersistedState = () => localStorage.removeItem(stateKey);
