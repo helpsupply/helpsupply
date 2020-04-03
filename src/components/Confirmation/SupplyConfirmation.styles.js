b4449dc889b5a0da41724d177d28259390d45d32
Josh Rounsville <joshuarounsville@gmail.com>
2020-04-02 19:31:02 -0700

    chore(supply): add new supply confirmation page

---
+++ src/components/Confirmation/SupplyConfirmation.styles.js
@@ -0,0 +1,13 @@
+import { css } from 'lib/utils/media-queries';
+import { Space } from 'lib/theme';
+
+export const styles = {
+  button: css({
+    marginTop: ['auto', '', Space.S5],
+  }),
+  subCta: css({
+    marginTop: Space.S20,
+  }),
+};
+
+export default styles;
