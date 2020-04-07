2c40ed28602e70693ef9fa65846a825d8dc5f6e7
davydka <davydka@gmail.com>
2020-04-07 12:46:53 -0700

    feat: update signup confirmation

--- src/pages/signup_confirmation.js
+++ src/pages/signup_confirmation.js
@@ -1,18 +1,29 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { useTranslation } from 'react-i18next';
+import ReactMarkdown from 'react-markdown';
 
 import Page from 'components/layouts/Page';
 import HeaderInfo from 'components/Form/HeaderInfo';
 
-function SignUp(props) {
+function SignUp({ backend }) {
   const { t } = useTranslation();
 
+  const email = backend.getEmailForSignIn()
+    ? `**${backend.getEmailForSignIn()}**`
+    : t('request.workEmailForm.sent.description.emailDefault');
+
   return (
-    <Page hasBackButton={false}>
+    <Page>
       <HeaderInfo
         title={t('request.workEmailForm.sent.title')}
-        description={t('request.workEmailForm.sent.description')}
+        description={
+          <ReactMarkdown
+            source={t('request.workEmailForm.sent.description', {
+              email,
+            })}
+          />
+        }
       />
     </Page>
   );
