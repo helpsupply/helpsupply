233413a3202bcbaa90bacd383617aa6e55a6d0a4
Liz Banach <erbanach@gmail.com>
2020-04-18 20:42:49 -0700

    feat: add meta data + update favicon

---
+++ src/components/layouts/Page/MetaData.js
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { Helmet } from 'react-helmet';
+
+const MetaData = () => (
+  <Helmet>
+    <meta
+      property="og:description"
+      content="Connecting healthcare workers to volunteer-powered grocery shopping, childcare and mental health services."
+    />
+    <meta property="og:url" content="http://help.supply" />
+    <meta property="og:site_name" content="Help Supply" />
+    <meta property="og:image" content="http://" />
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:site" content="@HelpSupply" />
+    <meta name="twitter:image:alt" content="Help Supply Logo" />
+    <meta name="twitter:image:alt" content="Help Supply Logo" />
+    <meta property="fb:app_id" content="" />
+  </Helmet>
+);
+
+export default MetaData;
