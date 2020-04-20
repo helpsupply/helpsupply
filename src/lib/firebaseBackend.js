042e3a685575ae3f9423171b7009f7ac3303e3bf
Ben Newhouse <newhouseb@gmail.com>
2020-04-20 13:26:30 -0400

    Fixing up environment selection

--- src/lib/firebaseBackend.js
+++ src/lib/firebaseBackend.js
@@ -4,28 +4,34 @@ const Firebase = require('firebase');
 const axios = require('axios').default;
 
 let config = null;
-if (process.env.REACT_APP_HOST_ENV === 'production') {
-  config = {
-    apiKey: 'AIzaSyBtHRUSPc0e4rF057rotTeVpeZ3lDWoQTE',
-    authDomain: 'hospitalcommunity.firebaseapp.com',
-    databaseURL: 'https://hospitalcommunity.firebaseio.com',
-    projectId: 'hospitalcommunity',
-    storageBucket: 'hospitalcommunity.appspot.com',
-    messagingSenderId: '726825294724',
-    appId: '1:726825294724:web:f3dc998705fbe6c8b09bd1',
-    measurementId: 'G-987WXEFK5D',
-  };
-} else {
-  config = {
-    apiKey: 'AIzaSyAXmpRfgvne_w2apeWb3Q8wSNUqNey3-Mg',
-    authDomain: 'help-supply-staging.firebaseapp.com',
-    databaseURL: 'https://help-supply-staging.firebaseio.com',
-    projectId: 'help-supply-staging',
-    storageBucket: 'help-supply-staging.appspot.com',
-    messagingSenderId: '395198068697',
-    appId: '1:395198068697:web:3a7975e877fbc98c190bc8',
-    measurementId: 'G-5SYKT9JHQF',
-  };
+let functionurl = null;
+
+if (typeof window !== 'undefined') {
+  if (window.location.hostname === 'help.supply') {
+    functionurl = 'https://us-central1-hospitalcommunity.cloudfunctions.net';
+    config = {
+      apiKey: 'AIzaSyBtHRUSPc0e4rF057rotTeVpeZ3lDWoQTE',
+      authDomain: 'hospitalcommunity.firebaseapp.com',
+      databaseURL: 'https://hospitalcommunity.firebaseio.com',
+      projectId: 'hospitalcommunity',
+      storageBucket: 'hospitalcommunity.appspot.com',
+      messagingSenderId: '726825294724',
+      appId: '1:726825294724:web:f3dc998705fbe6c8b09bd1',
+      measurementId: 'G-987WXEFK5D',
+    };
+  } else {
+    functionurl = 'https://us-central1-help-supply-staging.cloudfunctions.net';
+    config = {
+      apiKey: 'AIzaSyAXmpRfgvne_w2apeWb3Q8wSNUqNey3-Mg',
+      authDomain: 'help-supply-staging.firebaseapp.com',
+      databaseURL: 'https://help-supply-staging.firebaseio.com',
+      projectId: 'help-supply-staging',
+      storageBucket: 'help-supply-staging.appspot.com',
+      messagingSenderId: '395198068697',
+      appId: '1:395198068697:web:3a7975e877fbc98c190bc8',
+      measurementId: 'G-5SYKT9JHQF',
+    };
+  }
 }
 
 export default class FirebaseBackend {
@@ -640,8 +646,7 @@ export default class FirebaseBackend {
       method: 'POST',
       // Equal to EMAIL_SIGNUP_COMPLETE
       // TODO: Configure staging vs production somewhere
-      url:
-        'https://us-central1-help-supply-staging.cloudfunctions.net/sendSigninEmail',
+      url: functionurl + '/sendSigninEmail',
       params: {
         email: email,
         url: `${window.location.protocol}//${window.location.host}/signup/complete/${zip}/`,
@@ -772,8 +777,7 @@ export default class FirebaseBackend {
     await axios({
       method: 'POST',
       // TODO: Configure staging vs production somewhere
-      url:
-        'https://us-central1-help-supply-staging.cloudfunctions.net/sendRequestConfirmation',
+      url: functionurl + '/sendRequestConfirmation',
       params: {
         organization: data.organization,
         request: data.id,
