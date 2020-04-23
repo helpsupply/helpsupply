b8cbe5a38b72fe0430cd034067e94d3659b4a224
davydka <davydka@gmail.com>
2020-04-23 14:27:09 -0700

    hotfix: no capitalize undefined

--- src/lib/utils/strings.js
+++ src/lib/utils/strings.js
@@ -2,7 +2,7 @@
  * Convert string to new string with capitlized first letter
  */
 export const capitalize = (str) =>
-  `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`;
+  str && `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`;
 
 export const formatPhoneNumber = (phone) => {
   const numericString = phone.replace(/[^0-9]+/g, '');
