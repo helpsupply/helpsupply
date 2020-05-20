d6231bc909a9351fbf363cdf8bdd303b771864a2
davydka <davydka@gmail.com>
2020-05-19 19:48:17 -0700

    fix: welcome screen had wrong color in description

--- src/pages/signup_welcome.js
+++ src/pages/signup_welcome.js
@@ -4,12 +4,15 @@ import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 
 import { Routes } from 'lib/constants/routes';
+import { Color, Space } from 'lib/theme';
 
 import Page from 'components/layouts/Page';
 import ConfirmationWrapper from 'components/Confirmation/ConfirmationWrapper';
 import Text from 'components/Text';
 import { TEXT_TYPE } from 'components/Text/constants';
 import { PrimaryButton } from 'components/Button';
+
+const styles = { color: Color.GRAY_75, marginBottom: Space.S50 };
 
 function LoginWelcome() {
   const history = useHistory();
@@ -22,7 +25,7 @@ function LoginWelcome() {
   return (
     <Page>
       <ConfirmationWrapper noIcon title={t('welcome.signup.title')}>
-        <Text as="p" type={TEXT_TYPE.BODY_2}>
+        <Text as="p" type={TEXT_TYPE.BODY_2} css={styles}>
           {t('welcome.signup.description')}
         </Text>
         <PrimaryButton onClick={handleSubmit}>
